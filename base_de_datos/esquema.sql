2eb3d00b86df05444e6145e3345c36e34c51357d
Javier GF <javierguizadofarje@gmail.com>
2026-09-05 12:59:20 -0500

    Agregar socios comerciales con carrusel

--- base_de_datos/esquema.sql
+++ base_de_datos/esquema.sql
@@ -61,6 +61,15 @@ CREATE TABLE IF NOT EXISTS banners_publicitarios (
   fecha_actualizacion TEXT
 );
 
+CREATE TABLE IF NOT EXISTS socios_comerciales (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nombre TEXT NOT NULL,
+  imagen_url TEXT NOT NULL,
+  activo INTEGER NOT NULL DEFAULT 1 CHECK (activo IN (0, 1)),
+  fecha_creacion TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  fecha_actualizacion TEXT
+);
+
 CREATE TABLE IF NOT EXISTS puntos_atencion (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   nombre TEXT NOT NULL,
